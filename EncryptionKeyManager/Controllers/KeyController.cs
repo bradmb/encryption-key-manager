81757317bfcf603edc29aa050a7965e4e556a609
Brad Butner <bbutner@take3tech.com>
2018-05-09 15:59:03 -0600

    Added base code

---
+++ EncryptionKeyManager/Controllers/KeyController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EncryptionKeyManager.Controllers
+{
+    [Route("api/[controller]")]
+    public class KeyController : Controller
+    {
+        private readonly string KEY = Environment.GetEnvironmentVariable("APP_ENCRYPTION_KEY");
+        private readonly string[] ALLOWED_HOST = Environment.GetEnvironmentVariable("APP_ALLOWED_HOSTS").Split(";");
+        private readonly string ALLOWED_HOST_STR = Environment.GetEnvironmentVariable("APP_ALLOWED_HOSTS");
+
+        /// <summary>
+        /// Returns the encryption key
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public string Get()
+        {
+            var ipAddress = Request.HttpContext.Connection.RemoteIpAddress.ToString();
+            if (ALLOWED_HOST.Contains(ipAddress) || ALLOWED_HOST.Contains("*"))
+            {
+                return KEY;
+            }
+
+            return null;
+        }
+    }
+}
